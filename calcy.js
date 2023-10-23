ae9bd3695500280e3d92a650842860c7dd509702
HarshiyaShaik14 <147414062+HarshiyaShaik14@users.noreply.github.com>
2023-10-23 15:25:20 +0530

    JavaScript

---
+++ calcy.js
@@ -0,0 +1,21 @@
+// Calculator logic
+let displayValue = '';
+
+function addDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+function result() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
